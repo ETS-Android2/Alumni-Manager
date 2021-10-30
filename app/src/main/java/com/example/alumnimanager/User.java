5a7b4426915192931c287ad97de4f0f0533922aa
Priyanka1818 <vyshu.tadipaneni14@gmail.com>
2021-10-29 22:11:05 -0500

    added User java file

---
+++ app/src/main/java/com/example/alumnimanager/User.java
@@ -0,0 +1,37 @@
+package com.example.alumnimanager;
+
+public class User {
+    String uname;
+    String fname;
+    String password;
+
+    public User(String uname, String fname, String password) {
+        this.uname = uname;
+        this.fname = fname;
+        this.password = password;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
