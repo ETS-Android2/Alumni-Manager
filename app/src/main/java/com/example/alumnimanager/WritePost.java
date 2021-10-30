61dbe7daf2f76cc857abf609ca8f1641e07765ae
Priyanka1818 <priyankathambabathula1998@gmail.com>
2021-10-29 21:57:08 -0500

    added write post java file

--- app/src/main/java/com/example/alumnimanager/WritePost.java
+++ app/src/main/java/com/example/alumnimanager/WritePost.java
@@ -1,49 +1,26 @@
 package com.example.alumnimanager;
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity;
-=======
 
 import androidx.appcompat.app.AppCompatActivity;
-
->>>>>>> origin/master
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-<<<<<<< HEAD
 public class WritePost extends AppCompatActivity {
     Button backButton;
-=======
-
-public class WritePost extends AppCompatActivity {
-    Button backButton;
-
->>>>>>> origin/master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_post);
-<<<<<<< HEAD
         backButton = findViewById(R.id.backButton);
-=======
-
-        backButton = findViewById(R.id.backButton);
-
->>>>>>> origin/master
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(WritePost.this,
                         WallActivity.class);
                 //Intent is used to switch from one activity to another.
-<<<<<<< HEAD
                 startActivity(i);
-=======
-
-                startActivity(i);
-
->>>>>>> origin/master
             }
         });
     }
 }
+
