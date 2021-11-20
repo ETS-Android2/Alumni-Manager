cdd2ceddf51bfc46ac67acc28a2a9c0da54779c8
Priyanka1818 <priyankathambabathula1998@gmail.com>
2021-11-19 19:15:07 -0600

    added CommentsActivity.java

---
+++ app/src/main/java/com/example/alumnimanager/CommentsActivity.java
@@ -0,0 +1,60 @@
+package com.example.alumnimanager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.alumnimanager.controller.CommentHandler;
+
+import java.util.ArrayList;
+
+public class CommentsActivity extends AppCompatActivity {
+    private static final android.R.attr R = ;
+    EditText txtComment;
+    Button btnComment;
+    CommentHandler db;
+    ListView listComments;
+    String postID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+        Bundle b = getIntent().getExtras();
+        postID = b.getString("postID");
+        txtComment = findViewById(R.id.txtComment);
+        btnComment = findViewById(R.id.btnComment);
+        listComments = findViewById(R.id.listComments);
+        db = new CommentHandler(getApplicationContext());
+        ArrayList<String> list = db.fetchAllComments(postID);
+        ArrayAdapter<String> arr;
+        arr
+                = new ArrayAdapter<String>(
+                this,
+                R.layout.support_simple_spinner_dropdown_item,
+                list);
+        listComments.setAdapter(arr);
+
+
+        btnComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.addNewComment(postID, txtComment.getText().toString().trim(), getApplicationContext());
+                Toast.makeText(getApplicationContext(), "Comment Added", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(getApplicationContext(), CommentsActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                i.putExtra("postID", postID);
+                getApplicationContext().startActivity(i);
+            }
+        });
+
+    }
+}
