a8dad5950fbde636d4c4ae7b1e258d579b124640
Priyanka1818 <vyshu.tadipaneni14@gmail.com>
2021-11-19 19:42:18 -0600

    added utils java class

---
+++ app/src/main/java/com/example/alumnimanager/modal/Utils.java
@@ -0,0 +1,34 @@
+package com.example.alumnimanager.modal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+
+    private static User user;
+    public static String PREF_NAME = "ALUMINI_PREF";
+    public static void setUser(User u) {
+        user = u;
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+
+    public static String toTime(String mills) {
+        Long currentTime = Long.parseLong(mills);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
+        Date date = new Date(currentTime);
+        String time = simpleDateFormat.format(date);
+        return time;
+    }
+
+
+    public static String toDate(String mills) {
+        long currentDateTime =  Long.parseLong(mills);
+        //creating Date from millisecond
+        Date currentDate = new Date(currentDateTime);
+        return currentDate + "";
+    }
+
+}
